760d4cbfcbd7df3816dbba69ccbdf3a9c904ffe4
D Murphy <rexmicrosoft@yahoo.com.ph>
2019-05-10 09:18:52 +0800

    update

--- Mics.sql
+++ Mics.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v8.53 
-MySQL - 5.7.12 : Database - micsdb
+MySQL - 5.5.5-10.1.38-MariaDB : Database - micsdb
 *********************************************************************
 */
 
@@ -9,7 +9,6 @@ MySQL - 5.7.12 : Database - micsdb
 /*!40101 SET SQL_MODE=''*/;
 
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`micsdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
@@ -58,7 +57,7 @@ CREATE TABLE `addformtable` (
 
 /*Data for the table `addformtable` */
 
-insert  into `addformtable`(`AddFormId`,`ImagePath`,`EntryTitle`,`EntryDescription`,`Resolution`,`DateOfEntry`,`Author`,`SubCategoryID`) values (3,'uploads/bgtransnotext.png','mangkanr','mang','man','2017-04-14','2',1),(5,'uploads/bgtransnotext.png','1','1','1','2017-04-14','2',2),(6,'uploads/','sfdsfd','ada da da da dad ad da da da da','asdasd','2017-04-27','9',1),(9,'','gg','gg','ggg','2017-04-15','2',1),(10,'','gg','gg','gg','2017-04-15','2',1),(12,'uploads/','gg','gg','gg','2017-04-15','2',1),(13,'uploads/no-photo.jpg','asd','d','sdasd','2017-04-15','2',1),(15,'uploads/no-photo.jpg','flashdrive','gwapo','ghhh','2017-04-16','2',3),(16,'uploads/no-photo.jpg','malfuntion','My mouse is so cute and I distroyed it','I need a new and awesome mouse','2017-04-19','4',2),(18,'uploads/no-photo.jpg','sss','zzz','a','2017-04-24','2',1),(19,'uploads/no-photo.jpg','werwer','wer','ererere','2017-04-25','2',1),(20,'uploads/no-photo.jpg','ssf','sf','sf','2017-04-28','2',1);
+insert  into `addformtable`(`AddFormId`,`ImagePath`,`EntryTitle`,`EntryDescription`,`Resolution`,`DateOfEntry`,`Author`,`SubCategoryID`) values (3,'uploads/bgtransnotext.png','mangkanr','mang','man','2017-04-14','2',1),(5,'uploads/bgtransnotext.png','1','1','1','2017-04-14','2',2),(6,'uploads/','sfdsfd','ada da da da dad ad da da da da','asdasd','2017-04-27','9',1),(9,'','gg','gg','ggg','2017-04-15','2',1),(13,'uploads/no-photo.jpg','asd','d','sdasd','2017-04-15','2',1),(15,'uploads/no-photo.jpg','flashdrive','gwapo','ghhh','2017-04-16','2',3),(16,'uploads/no-photo.jpg','malfuntion','My mouse is so cute and I distroyed it','I need a new and awesome mouse','2017-04-19','4',2),(18,'uploads/no-photo.jpg','sss','zzz','a','2017-04-24','2',1),(19,'uploads/no-photo.jpg','werwer','wer','ererere','2017-04-25','2',1),(20,'uploads/no-photo.jpg','ssf','sf','sf','2017-04-28','2',1);
 
 /*Table structure for table `categorytable` */
 
@@ -97,6 +96,19 @@ CREATE TABLE `editformtable` (
 
 insert  into `editformtable`(`EditId`,`ImagePath`,`EntryTitle`,`EntryDescription`,`Resolution`,`DateOfEdit`,`Author`,`EditDescription`,`AddFormId`) values (1,'uploads/bgtransnotext.png','mangkanr','mang','man','2017-04-14','2','ma',3),(2,'uploads/bgtrans.png','asd','drex','drexron','2017-04-15','2','',14),(3,'uploads/Lake-pier-mountains-sky-stars-night_1920x1080.jpg','asd','drex','drex','2017-04-15','2','',14),(4,'uploads/Lake-pier-mountains-sky-stars-night_1920x1080.jpg','asd','drex','drexronc','2017-04-15','2','',14),(5,'uploads/Lake-pier-mountains-sky-stars-night_1920x1080.jpg','et','ed','rd','2017-04-15','2','',14),(6,'uploads/Lake-pier-mountains-sky-stars-night_1920x1080.jpg','et','ed','rd','2017-04-15','2','xx',14),(7,'uploads/edited.png','et1a','entrya','resa','2017-04-15','3','bag-0',2),(8,'uploads/jon.jpg','got','Tits and wines','Where are the Gods of tits and wines?','2017-04-20','2','',17),(9,'uploads/no-photo.jpg','aaaa','','','2017-04-24','','',17),(10,'uploads/','sfdsfd','ada da da da dad ad da da da da','asdasd','2017-04-27','9','',6);
 
+/*Table structure for table `problemtable` */
+
+DROP TABLE IF EXISTS `problemtable`;
+
+CREATE TABLE `problemtable` (
+  `ProblemId` int(6) NOT NULL AUTO_INCREMENT,
+  `ProblemName` varchar(60) DEFAULT NULL,
+  `AddFormId` int(6) DEFAULT NULL,
+  PRIMARY KEY (`ProblemId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `problemtable` */
+
 /*Table structure for table `subcategorytable` */
 
 DROP TABLE IF EXISTS `subcategorytable`;
@@ -112,7 +124,7 @@ CREATE TABLE `subcategorytable` (
 
 /*Data for the table `subcategorytable` */
 
-insert  into `subcategorytable`(`SubCategoryId`,`NameOfSubCategory`,`CategoryId`,`activate`,`datedisactivate`) values (1,'System Unit',1,1,NULL),(2,'Mouse',1,1,NULL),(3,'Keyboard',1,1,NULL),(4,'Speaker',1,1,NULL),(5,'Web cam',1,1,NULL),(6,'Mic',1,0,'2017-04-20 10:46:37'),(7,'Printer',1,0,'2017-04-28 11:05:45'),(8,'Scanner',1,0,'2017-04-21 10:51:21'),(9,'Flash Drive',1,0,'2017-04-20 10:52:53'),(10,'External Drive',1,0,'2017-04-28 11:04:37'),(11,'Operating System',2,1,NULL),(12,'Microsoft Office',2,1,NULL),(13,'Virus',2,1,NULL),(14,'Blue Screen ',2,1,NULL),(15,'Drivers',2,1,NULL),(16,'No Internet',3,1,NULL),(18,'dasdads',6,0,'2017-04-21 10:45:22'),(19,NULL,NULL,0,NULL),(20,NULL,NULL,0,NULL),(21,'',5,1,NULL),(22,'anything',6,1,NULL),(23,'asdadasdasdasdaadad',6,0,'2017-04-28 10:21:35'),(24,'anything particular',6,1,NULL),(25,'anythin at all',6,1,NULL),(26,'fiore error',6,1,NULL),(27,'',6,0,'2017-04-28 10:25:06'),(28,'',6,0,'2017-04-28 10:26:58'),(29,'',6,0,'2017-04-28 10:27:49'),(30,'',6,0,'2017-04-28 10:28:11'),(31,'',6,0,'2017-04-28 10:31:31'),(32,'something',3,1,NULL),(33,'printer',2,1,NULL),(34,'',1,1,NULL),(35,'printerssss',1,1,NULL),(36,'',2,0,'2017-04-28 13:18:35');
+insert  into `subcategorytable`(`SubCategoryId`,`NameOfSubCategory`,`CategoryId`,`activate`,`datedisactivate`) values (1,'System Unit',1,1,NULL),(2,'Mouse',1,1,NULL),(3,'Keyboard',1,1,NULL),(4,'Speaker',1,1,NULL),(5,'Web cam',1,1,NULL),(6,'Mic',1,0,'2017-04-20 10:46:37'),(7,'Printer',1,0,'2017-04-28 11:05:45'),(8,'Scanner',1,0,'2017-04-21 10:51:21'),(9,'Flash Drive',1,0,'2017-04-20 10:52:53'),(10,'External Drive',1,0,'2017-04-28 11:04:37'),(11,'Operating System',2,1,NULL),(12,'Microsoft Office',2,1,NULL),(13,'Virus',2,1,NULL),(14,'Blue Screen ',2,1,NULL),(15,'Drivers',2,1,NULL),(16,'No Internet',3,1,NULL),(18,'dasdads',6,0,'2017-04-21 10:45:22'),(19,NULL,NULL,0,NULL),(20,NULL,NULL,0,NULL),(21,'',5,1,NULL),(22,'anything',6,1,NULL),(23,'asdadasdasdasdaadad',6,0,'2017-04-28 10:21:35'),(24,'anything particular',6,1,NULL),(25,'anythin at all',6,1,NULL),(26,'fiore error',6,0,'2019-05-10 08:36:11'),(27,'',6,0,'2017-04-28 10:25:06'),(28,'',6,0,'2017-04-28 10:26:58'),(29,'',6,0,'2017-04-28 10:27:49'),(30,'',6,0,'2017-04-28 10:28:11'),(31,'',6,0,'2017-04-28 10:31:31'),(32,'something',3,1,NULL),(33,'printer',2,0,'2019-05-10 08:35:53'),(34,'',1,0,'2019-05-10 08:35:31'),(35,'Printer',1,1,NULL),(36,'',2,0,'2017-04-28 13:18:35');
 
 /*Table structure for table `view_edit_history` */
 
@@ -193,6 +205,5 @@ DROP TABLE IF EXISTS `view_subcategory`;
 /*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_subcategory` AS select `subcategorytable`.`SubCategoryId` AS `SubCategoryId`,`subcategorytable`.`NameOfSubCategory` AS `NameOfSubCategory`,`subcategorytable`.`CategoryId` AS `CategoryId`,`subcategorytable`.`activate` AS `activate`,`categorytable`.`NameOfCategory` AS `NameOfCategory` from (`categorytable` join `subcategorytable` on((`categorytable`.`CategoryId` = `subcategorytable`.`CategoryId`))) */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
