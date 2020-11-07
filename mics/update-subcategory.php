17642d91450fe55b6f64642ef0d5c1dc39308b96
D Murphy <rexmicrosoft@yahoo.com.ph>
2020-11-07 20:00:42 +0800

    delete folder

--- mics/update-subcategory.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('connection.php');
-
-
-
-mysqli_query($connection, "update subcategorytable set NameOfSubCategory = '" . $_POST['NameOfSubCategory'] . "' where SubCategoryId = '" . $_POST['SubCategoryId'] . "'");
-
-header("Location: add-edit-delete-subcategory.php?CategoryId=".$_POST['CategoryId']);
- ?>
