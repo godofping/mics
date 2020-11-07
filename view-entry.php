1db460b539fa041a99b99e6176ec95e7af58958a
D Murphy <rexmicrosoft@yahoo.com.ph>
2020-11-07 20:23:41 +0800

    upss

--- view-entry.php
+++ view-entry.php
@@ -8,20 +8,15 @@ $getUserRes = mysqli_fetch_assoc($getUserQry);
 <!DOCTYPE html>
 <html lang="en">
 
-<!--================================================================================
-  Item Name: Materialize - Material Design Admin Template
-  Version: 1.0
-  Author: GeeksLabs
-  Author URL: http://www.themeforest.net/user/geekslabs
-================================================================================ -->
+
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="msapplication-tap-highlight" content="no">
-    <meta name="description" content="Materialize is a Material Design Admin Template,It's modern, responsive and based on Material Design by Google. ">
-    <meta name="keywords" content="materialize, admin template, dashboard template, flat admin template, responsive admin template,">
+    <meta name="description" content="MICS Knowledge Base System">
+    <meta name="keywords" content="MICS Knowledge Base System">
     <title>MICS</title>
 
     <!-- Favicons-->
@@ -355,7 +350,7 @@ $getUserRes = mysqli_fetch_assoc($getUserQry);
 
     <!-- chartjs -->
     <script type="text/javascript" src="js/plugins/chartjs/chart.min.js"></script>
-    <script type="text/javascript" src="js/plugins/chartjs/chart-script.js"></script>
+    
 
     <!-- sparkline -->
     <script type="text/javascript" src="js/plugins/sparkline/jquery.sparkline.min.js"></script>
